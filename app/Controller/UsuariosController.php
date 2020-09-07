f42245a7d9e3e4f65f0bb8bc75eea8e22b91b5b5
Alvaro <alvaro99.cunha@gmail.com>
2020-09-07 00:55:38 -0300

    deleted some unused files

--- app/Controller/UsuariosController.php
+++ app/Controller/UsuariosController.php
@@ -10,15 +10,4 @@
 		
 	}
 
-<<<<<<< HEAD
 ?>
-=======
-	public function add(){
-		//register new user
-		if($this->request->is('post')){
-			pr($this->request->data)
-		}
-	}
-	
-}
->>>>>>> 9248a48741d318db54730db7f1486a31947f2513
